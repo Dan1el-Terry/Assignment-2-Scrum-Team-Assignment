8b05af195c4ecdab24e1b47a49b70bc22a9b6cd7
Gene Lewis <Gene Lewis@GENELEWIS>
2026-08-17 14:15:02 +1200

    Added basic wpf login window.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows;
+
+namespace Assign_2
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            MainWindow window = new MainWindow();
+
+            app.Run(window);
+        }
+    }
+}
--- obj/Debug/net8.0/Assign 2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assign 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assign 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3901f7fa23e6315fd8cb87323f0127354f6cde13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assign 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assign 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
